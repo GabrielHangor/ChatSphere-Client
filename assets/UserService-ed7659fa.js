d9fb668aef14e53685002a1c21571fa77d802996
Gabriel <gaba93@yandex.ru>
2023-04-04 17:14:02 +0300

    Updates

--- assets/UserService-ed7659fa.js
+++
@@ -1,1 +0,0 @@
-import{b4 as a,b1 as e}from"./index-7ca99844.js";class c{static async createUser(s){await a.post("/users",s),await e.login({email:s.email,password:s.password})}static async getAllUsers(){const{data:s}=await a.get("/users");return s}static async getUsersByName(s){const{data:t}=await a.get(`/users/${s}`);return t}}export{c as U};
